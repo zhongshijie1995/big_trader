e7eeb3cbe4c3a2c0180754835767a50d63a43964
zhongshijie <zhongshijie1995@outlook.com>
2021-12-26 23:45:45 +0800

    存储方式改为使用数据库

--- _collects.py
+++ _collects.py
@@ -1,75 +1,72 @@
-import os
 import time
-import shutil
 import typing
 
+from sqlalchemy import create_engine
 from tqdm import tqdm
 
 import pandas as pd
 import efinance as ef
 
 
-def get_today_min_cut_by_stock_code(today: str, stock_code: str) -> pd.DataFrame:
-    """
-    获取一支股票当天的分钟状况
+class Collector(object):
+    def __init__(self, engine_str: str = 'sqlite+pysqlite:///db.sqlite'):
+        self.engine = create_engine(engine_str)
 
-    :param today:
-    :param stock_code:
-    :return:
-    """
-    # 获取1分钟K线
-    a = ef.stock.get_quote_history(stock_code, beg=today, end=today, klt=1)
-    a = a.rename(columns={'日期': '时间'})
-    # 获取1分钟交易单
-    b = ef.stock.get_today_bill(stock_code)
-    # 合并分钟状况
-    c = pd.merge(a, b, on=['股票名称', '股票代码', '时间'])
-    return c
+    @staticmethod
+    def get_today_min_cut_by_stock_code(the_day: str, stock_code: str) -> pd.DataFrame:
+        """
+        获取一支股票当天的分钟状况
 
+        :param the_day:
+        :param stock_code:
+        :return:
+        """
+        # 获取1分钟K线
+        a = ef.stock.get_quote_history(stock_code, beg=the_day, end=the_day, klt=1)
+        a = a.rename(columns={'日期': '时间'})
+        # 获取1分钟交易单
+        b = ef.stock.get_today_bill(stock_code)
+        # 合并分钟状况
+        c = pd.merge(a, b, on=['股票名称', '股票代码', '时间'])
+        return c
 
-def get_today_min_cut_by_stock_list(today: str, stock_code_list: typing.List[str]) -> pd.DataFrame:
-    """
-    获取多支股票当天的分钟状况
+    @staticmethod
+    def get_real_time_summary() -> pd.DataFrame:
+        """
+        获取全市场实时概况
 
-    :param today:
-    :param stock_code_list:
-    :return:
-    """
-    # 遍历所有股票代码用于获取分钟状况
-    result = None
-    for stock_code in tqdm(stock_code_list):
-        tmp = get_today_min_cut_by_stock_code(today, stock_code)
-        if result is None:
-            result = tmp
-        else:
-            result = result.append(tmp)
-    return result
+        :return:
+        """
+        return ef.stock.get_realtime_quotes()
 
+    def get_today_min_cut_by_stock_list(self, today: str, stock_code_list: typing.List[str]) -> pd.DataFrame:
+        """
+        获取多支股票当天的分钟状况
 
-def get_real_time_summary() -> pd.DataFrame:
-    """
-    获取全市场实时概况
+        :param today:
+        :param stock_code_list:
+        :return:
+        """
+        # 遍历所有股票代码用于获取分钟状况
+        result = None
+        for stock_code in tqdm(stock_code_list):
+            tmp = self.get_today_min_cut_by_stock_code(today, stock_code)
+            if result is None:
+                result = tmp
+            else:
+                result = result.append(tmp)
+        return result
 
-    :return:
-    """
-    return ef.stock.get_realtime_quotes()
+    def save_today_at_night(self) -> None:
+        """
+        夜间保存当日发生数据
 
+        :return:
+        """
+        today = time.strftime('%Y%m%d', time.localtime())
 
-def save_today_at_night(save_path: str = 'data') -> None:
-    """
-    夜间保存当日发生数据
+        a = self.get_real_time_summary()
+        a.to_sql('stock_summary', self.engine, index=False, if_exists='append')
 
-    :return:
-    """
-    today = time.strftime('%Y%m%d', time.localtime())
-    save_path = os.path.abspath(os.path.join(save_path, today))
-
-    if os.path.exists(save_path):
-        shutil.rmtree(save_path)
-    os.makedirs(save_path)
-
-    a = get_real_time_summary()
-    a.to_csv(os.path.join(save_path, 'summary.csv'), index=False)
-
-    b = get_today_min_cut_by_stock_list(today, a['股票代码'])
-    b.to_csv(os.path.join(save_path, 'min_cuts.csv'), index=False)
+        b = self.get_today_min_cut_by_stock_list(today, a['股票代码'])
+        b.to_sql('stock_detail', self.engine, index=False, if_exists='append')
--- main.py
+++ main.py
@@ -1,8 +1,5 @@
 import _collects
 
-
-DATA_PATH = '/Users/zhongshijie/Desktop/99-Drive/Data/01-big_trader'
-
-
 if __name__ == '__main__':
-    _collects.save_today_at_night(DATA_PATH)
+    collector = _collects.Collector()
+    collector.save_today_at_night()
